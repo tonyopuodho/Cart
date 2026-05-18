3998824b6b90622c1095837d3324c11364c85622
tonyopuodho <tonyopuodho@gmail.com>
2026-05-18 10:03:56 +0300

    A function that fetches products from an api

--- src/Components/Products.jsx
+++ src/Components/Products.jsx
@@ -1,7 +1,11 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import NavigationBar from './Navigation'
+import { ShoppingCartContext } from '../context/Context'
 
 function Products() {
+    const {loading, products } = useContext(ShoppingCartContext)
+    if (loading) return <h1>Products loading please wait</h1>
+    console.log(products) 
   return (
     <div>
         <NavigationBar/>
--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,13 +4,20 @@ export const ShoppingCartContext = createContext(null)
 
 function ShoppingCartProvider({children}){
     const [products,setProducts] = useState([])
+    const [loading,setLoading] = useState(true)
     async function fetchProducts(){
         const apiRequest = await fetch("https://dummyjson.com/products")
         const apiResponse = await apiRequest.json()
-        console.log(apiResponse)
+        
+        if (apiResponse && apiResponse?.products) {
+            setProducts(apiResponse?.products)
+            setLoading(false)
+        }
     }
+
+    
     return (
-        <ShoppingCartContext.Provider value={{}}>{children}</ShoppingCartContext.Provider>
+        <ShoppingCartContext.Provider value={{ products,loading }}>{children}</ShoppingCartContext.Provider>
     )
 }
 
