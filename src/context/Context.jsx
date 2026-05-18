be8c05faa41deb38177a43307353d4cd62552a4a
tonyopuodho <tonyopuodho@gmail.com>
2026-05-18 09:52:51 +0300

    Changing the color pallets of navigation bar

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -1,8 +1,14 @@
-import { createContext } from "react";
+import { createContext, useState } from "react";
 
 export const ShoppingCartContext = createContext(null)
 
 function ShoppingCartProvider({children}){
+    const [products,setProducts] = useState([])
+    async function fetchProducts(){
+        const apiRequest = await fetch("https://dummyjson.com/products")
+        const apiResponse = await apiRequest.json()
+        console.log(apiResponse)
+    }
     return (
         <ShoppingCartContext.Provider value={{}}>{children}</ShoppingCartContext.Provider>
     )
