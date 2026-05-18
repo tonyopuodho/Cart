67ad49bf6bd382c53aff4a9f1e775ef8c8fdf6de
tonyopuodho <tonyopuodho@gmail.com>
2026-05-18 09:35:47 +0300

    Adding navigation bar

--- src/Components/Navigation.jsx
+++ src/Components/Navigation.jsx
@@ -3,9 +3,12 @@ import { FaShoppingCart } from 'react-icons/fa'
 
 function NavigationBar() {
   return (
-   <div>
-     
-   </div>
+    <div className='w-full h-[10vh] fixed top-0 bg-gray-300 shadow-md p-2 flex items-center'>
+        <div className='max-w-7xl w-full px-2 md:w-[80%] mx-auto flex justify-between items-center'>
+            <h1 className='font-extrabold text-3xl text-white'>Shopping Cart</h1>
+            <FaShoppingCart className='text-2xl text-white'/>
+        </div>
+    </div>
   )
 }
 
