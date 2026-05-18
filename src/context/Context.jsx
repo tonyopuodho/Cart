12dc890060de18e8b9d9bf3b0acfb733379e908c
tonyopuodho <tonyopuodho@gmail.com>
2026-05-18 09:45:36 +0300

    Implementing react context

---
+++ src/context/Context.jsx
@@ -0,0 +1,11 @@
+import { createContext } from "react";
+
+export const ShoppingCartContext = createContext(null)
+
+function ShoppingCartProvider({children}){
+    return (
+        <ShoppingCartContext.Provider value={}>{children}</ShoppingCartContext.Provider>
+    )
+}
+
+export default ShoppingCartProvider
