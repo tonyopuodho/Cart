bb73782b8915b95e82492d0f36cc1124aef4deaa
tonyopuodho <tonyopuodho@gmail.com>
2026-05-19 18:36:36 +0300

    Adding functionality to basket

--- src/Components/Cart.jsx
+++ src/Components/Cart.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { ShoppingCartContext } from '../context/Context'
+import CartTile from './CartTile'
+import { useNavigate } from 'react-router-dom'
 
 function Cart() {
+  const {cartItems } = useContext(ShoppingCartContext)
+  const navigate = useNavigate()
   return (
     <div className='max-w-5xl mx-auto max-md:max-w-xl py-4'>
        <h1 className='text-2xl font-extrabold text-center'>My cart page</h1>
        <div className='grid md:grid-cols-3 gap-12 mt-12'>
-        <div className='col-span-2 space-y-4'></div>
+        <div className='col-span-2 space-y-4'>
+          {
+            cartItems?.length ?
+            cartItems.map((item) => <CartTile item={item}/>)
+            : <h1>No items found</h1>
+          }
+        </div>
+        <div className='bg-gray-100 rounded-sm p-4 h-max w-full'>
+                <h1 className='text-xl font-extrabold border-b'>Order summary</h1>
+                <ul className='mt-4'>
+                    <p className='flex flex-wrap gap-4 text-sm font-bold'>Total
+                        <span>{cartItems.reduce((acc,curr) => acc + curr.totalPrice,0).toFixed(2)}</span>
+                    </p>
+                </ul>
+                <div className='flex justify-center gap-2 mt-8'>
+                    <button disabled={cartItems.length === 0} className='disabled:opacity-40 py-2 px-6 bg-black text-white cursor-pointer rounded-md text-sm'>Checkout</button>
+                     <button className='py-2 px-6 bg-black text-white cursor-pointer rounded-md text-sm' onClick={() => navigate("/")}>Continue shopping</button>
+                </div>
+          </div>
        </div>
     </div>
   )
--- src/Components/CartTile.jsx
+++ src/Components/CartTile.jsx
@@ -1,11 +1,34 @@
 import React, { Fragment, useContext } from 'react'
 import { ShoppingCartContext } from '../context/Context'
 
-function CartTile() {
-    const { cartItems } = useContext(ShoppingCartContext)
+function CartTile({item}) {
+    const { handleAddItemsTocart,cartItems} = useContext(ShoppingCartContext)
   return (
     <Fragment>
-        
+        <div className='grid grid-cols-3 items-start gap-5'>
+            <div className='col-span-2 flex items-start gap-4'>
+            <div className='w-20 h-20 bg-gray-500 p-2 rounded-md'>
+                <img src={item?.thumbnail} alt="" className='w-full h-full object-cover' />
+            </div>
+            <div>
+                <h1 className='text-base font-bold text-gray-900'>{item?.title}</h1>
+                <button className='py-2 px-6 bg-black text-white cursor-pointer rounded-md text-sm'>Remove</button>
+            </div>
+        </div>
+         <div className='ml-auto'>
+            <h3 className='text-gray-500 text-xl font-bold'>
+                ${item?.totalPrice}
+            </h3>
+            <p className='font-extrabold text-gray-800'>Quantity: {item?.quantity}</p>
+            <div className='mt-2 flex gap-2'>
+                <button
+                disabled = {item.quantity === 1}
+                className='disabled:opacity-40 border px-4 cursor-pointer'>-</button>
+                <button onClick={() => handleAddItemsTocart(item)} className='border px-4 cursor-pointer'>+</button>
+            </div>
+        </div>
+        </div>
+        <hr className='w-full'/>
     </Fragment>
   )
 }
